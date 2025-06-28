60955b9afdc7d54215f3469ebe697bbc823c1d8e
GayatriDevops <gayatrijyotsna.k@gmail.com>
2025-06-28 15:55:05 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://4.194.232.252";
-//const API_BASE_URL = "http://www.thegayatri.shop";
+//const API_BASE_URL = "http://4.194.232.252";
+const API_BASE_URL = "http://www.thegayatri.shop";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
